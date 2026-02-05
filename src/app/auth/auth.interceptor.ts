4e48a5b983979b2ebc5d9299e1bed7d85a7fa312
Ivan Marchenko <vania9023@gmail.com>
2026-02-05 18:12:57 +0000

    feat: enhance auth interceptor with token refresh logic and error handling

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,17 +1,53 @@
-import { HttpInterceptorFn } from "@angular/common/http";
+import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
 import { inject } from "@angular/core";
 import { Auth } from "./auth";
+import { catchError, switchMap, throwError } from "rxjs";
+import { TokenResponse } from "./auth-interface";
+
+let isRefreshing = false;
 
 export const authTokenInterceptor: HttpInterceptorFn = (req, next) => { 
-    const accessToken = inject(Auth).access_token;
+    const authService = inject(Auth);
+    const accessToken = authService.access_token;
 
     if(!accessToken) return next(req);
 
-    req = req.clone({
+    if(isRefreshing) { 
+        return refreshAndProceed(authService, req, next);
+    }
+
+    return next(addToken(req, accessToken))
+    .pipe(
+        catchError(error => {
+            // if refresh token is expired
+            if(error.status === 403) {
+                return refreshAndProceed(authService, req, next);
+            }
+            return throwError(() => error);
+        })
+    )
+}
+
+const refreshAndProceed = (authService: Auth, req: HttpRequest<any>, next: HttpHandlerFn) => {
+    if(!isRefreshing) { 
+        isRefreshing = true;
+
+        return authService.refreshAuthToken()
+        .pipe(
+            switchMap((res: TokenResponse) => {
+                isRefreshing = false;
+                return next(addToken(req, res.accessToken));
+            })
+        )
+    }
+    
+    return next(addToken(req, authService.access_token!));
+}
+
+const addToken = (req: HttpRequest<any>, accessToken: string) => {
+    return req.clone({
         setHeaders: {
             Authorization: `Bearer ${accessToken}`
         }
     })
-
-    return next(req);
 }
--- src/app/auth/auth.ts
+++ src/app/auth/auth.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
-import { tap } from 'rxjs';
+import { catchError, tap, throwError } from 'rxjs';
 import { TokenResponse } from './auth-interface';
 import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Injectable({
     providedIn: 'root',
@@ -13,6 +14,7 @@ export class Auth {
     http = inject(HttpClient);
     url = environment.apiUrl + '/Auth';
     cookieService = inject(CookieService);
+    router = inject(Router);
 
     access_token: string | null = null;
     refresh_token: string | null = null;
@@ -30,13 +32,44 @@ export class Auth {
             `${this.url}/login`, 
             payload
         ).pipe(
-            tap((res: TokenResponse) => { 
-                this.access_token = res.accessToken;
-                this.refresh_token = res.refreshToken;
-
-                this.cookieService.set('access_token', res.accessToken);
-                this.cookieService.set('refresh_token', res.refreshToken);
-            }
+            tap((res: TokenResponse) => this.saveTokens(res)
         ));
+    }
+
+    refreshAuthToken() {
+        return this.http.post<TokenResponse>(
+            `${this.url}/refresh-token`,
+            {
+                userId: this.getUserIdFromToken(this.access_token!),
+                refreshToken: this.cookieService.get('refresh_token')
+            }
+        ).pipe(
+            tap((res: TokenResponse) => this.saveTokens(res)),
+            catchError(err => {
+                this.logout();
+                return throwError(err);
+            })
+        )
+    }
+    
+    logout() { 
+        this.cookieService.delete('access_token');
+        this.cookieService.delete('refresh_token');
+        this.access_token = null;
+        this.refresh_token = null;
+        this.router.navigate(['/login']);
+    }
+
+    getUserIdFromToken(token: string): string { 
+        const payload = JSON.parse(atob(token.split('.')[1]));
+        return payload.UserId;
+    }
+
+    saveTokens(res: TokenResponse) {
+        this.access_token = res.accessToken;
+        this.refresh_token = res.refreshToken;
+
+        this.cookieService.set('access_token', res.accessToken);
+        this.cookieService.set('refresh_token', res.refreshToken);
     }
 }
