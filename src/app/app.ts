073f9f7cd26dd980113733b10c2e399824dadee6
Ivan Marchenko <vania9023@gmail.com>
2026-02-05 17:34:01 +0000

    Added footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './common-ui/header/header';
+import { Footer } from './common-ui/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
