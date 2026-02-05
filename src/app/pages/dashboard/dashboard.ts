5fc5856749eed4333cfe45ad5b28ee49fbe03e90
Ivan Marchenko <vania9023@gmail.com>
2026-02-05 17:25:55 +0000

    feat: add header component with navigation links and styles

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
-  imports: [],
-  templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+    selector: 'app-dashboard',
+    imports: [],
+    templateUrl: './dashboard.html',
+    styleUrl: './dashboard.css',
 })
-export class Dashboard {
 
+export class Dashboard {
+    
 }
