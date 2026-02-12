8c68532ead8b9683ca59f577fc64a17614a3da0e
Ivan Marchenko <vania9023@gmail.com>
2026-02-12 22:06:14 +0000

    feat: implement organization overview component with service and interfaces

---
+++ src/app/common-ui/organization-overview/organization-overview.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-overview',
+  imports: [],
+  templateUrl: './organization-overview.html',
+  styleUrl: './organization-overview.css',
+})
+export class OrganizationOverview {
+
+}
