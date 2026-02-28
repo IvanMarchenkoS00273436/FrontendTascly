8569ce618dd26aad1d7cea779d13f297975400b6
ryanmc1407 <ryanmcclelland@msn.com>
2026-02-28 03:03:53 +0000

    Added Ai task generator to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,20 +7,22 @@ import { WorkspaceMembers } from './common-ui/workspace-members/workspace-member
 import { UserProfile } from './common-ui/user-profile/user-profile';
 import { OrganizationOverview } from './common-ui/organization-overview/organization-overview';
 import { TasksKanbanView } from './common-ui/tasks-kanban-view/tasks-kanban-view';
+import { AIPromptComponent } from './pages/ai-prompt/ai-prompt';
 
 export const routes: Routes = [
     { path: 'login', component: Login },
     { path: 'register', component: Register },
-    { 
-        path: 'dashboard', 
-        component: Dashboard, 
+    {
+        path: 'dashboard',
+        component: Dashboard,
         canActivate: [canActivateAuth],
         children: [
             { path: '', redirectTo: 'organization', pathMatch: 'full' },
             { path: 'profile', component: UserProfile },
             { path: 'organization', component: OrganizationOverview },
             { path: ':id/members', component: WorkspaceMembers },
-            { path: 'projects/:id', component: TasksKanbanView }
+            { path: 'projects/:id', component: TasksKanbanView },
+            { path: 'ai-task-generator', component: AIPromptComponent }
         ]
     },
     { path: '', redirectTo: 'login', pathMatch: 'full' }
